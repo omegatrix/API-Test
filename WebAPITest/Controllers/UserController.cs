2b9030339c06cf1ca8c5b7531dd4c36869e438e7
omegatrix <arnoldbronson16@gmail.com>
2020-01-13 21:07:12 +0000

    API - Get users in London

---
+++ WebAPITest/Controllers/UserController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using Microsoft.AspNetCore.Mvc;
+using Nancy.Json;
+using WebAPITest.Models;
+
+namespace WebAPITest.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        // GET: api/User
+        [HttpGet]
+        public List<User> Get()
+        {
+            HttpWebRequest request = (HttpWebRequest) WebRequest.Create("https://bpdts-test-app.herokuapp.com/city/London/users");
+
+            WebResponse response = request.GetResponse();
+            string responseStream;
+
+            using (var streamReader = new StreamReader(response.GetResponseStream()))
+            {
+                responseStream = streamReader.ReadToEnd();
+            }
+
+            var serializer = new JavaScriptSerializer();
+            var users = serializer.Deserialize<List<User>>(responseStream);
+
+            return users;
+        }
+
+        [HttpGet]
+        public List<User> GetUsersWithinRange()
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create("https://bpdts-test-app.herokuapp.com/city/London/users");
+
+            WebResponse response = request.GetResponse();
+            string responseStream;
+
+            using (var streamReader = new StreamReader(response.GetResponseStream()))
+            {
+                responseStream = streamReader.ReadToEnd();
+            }
+
+            var serializer = new JavaScriptSerializer();
+            var users = serializer.Deserialize<List<User>>(responseStream);
+
+            return users;
+        }
+    }
+}
---
+++ WebAPITest/Models/User.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPITest.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string first_name { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string last_name { get; set; }
+
+        [StringLength(100)]
+        public  string email { get; set; }
+
+        [StringLength(15)]
+        public string ip_address { get; set; }
+
+        public double latitude { get; set; }
+
+        public double longitude { get; set; }
+    }
+}
