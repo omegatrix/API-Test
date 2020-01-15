e512ce2f005f3cae33e9c27807e68defc0c819e2
omegatrix <arnoldbronson16@gmail.com>
2020-01-15 20:24:47 +0000

    Added Get requests for get all users, get user by id and get users by city

--- WebAPITest/Controllers/UserController.cs
+++ WebAPITest/Controllers/UserController.cs
@@ -11,11 +11,10 @@ namespace WebAPITest.Controllers
     [ApiController]
     public class UserController : ControllerBase
     {
-        [Route("[controller]/WithinLondon")]
+        [Route("/WithinLondon")]
         [HttpGet]
         public ActionResult<User> GetAllUsersInLondon()
         {
-
             try
             {
                 List<User> users = null;
@@ -88,6 +87,111 @@ namespace WebAPITest.Controllers
             }
             
             return filteredUsers;
+        }
+
+        [Route("/AllUsers")]
+        [HttpGet]
+        public ActionResult<User> GetUsers()
+        {
+            try
+            {
+                List<User> users = null;
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create("https://bpdts-test-app.herokuapp.com/users");
+                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+                if (response.StatusCode == HttpStatusCode.OK)
+                {
+                    string responseStream;
+
+                    using (var streamReader = new StreamReader(response.GetResponseStream()))
+                    {
+                        responseStream = streamReader.ReadToEnd();
+                    }
+
+                    var serializer = new JavaScriptSerializer();
+                    users = serializer.Deserialize<List<User>>(responseStream);
+                   
+                    return Ok(users);
+                }
+            }
+
+            catch (Exception ex)
+            {
+
+                return NotFound();
+            }
+
+            return NoContent();
+        }
+
+        [Route("/User/{id}")]
+        [HttpGet]
+        public ActionResult<User> GetUserById(int id)
+        {
+            try
+            {
+                User user = null;
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create("https://bpdts-test-app.herokuapp.com/user/" + id.ToString());
+                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+                if (response.StatusCode == HttpStatusCode.OK)
+                {
+                    string responseStream;
+
+                    using (var streamReader = new StreamReader(response.GetResponseStream()))
+                    {
+                        responseStream = streamReader.ReadToEnd();
+                    }
+
+                    var serializer = new JavaScriptSerializer();
+                    user = serializer.Deserialize<User>(responseStream);
+
+                    return Ok(user);
+                }
+            }
+
+            catch (Exception ex)
+            {
+
+                return NotFound();
+            }
+
+            return NoContent();
+        }
+
+        [Route("/Users/{city}")]
+        [HttpGet]
+        public ActionResult<User> GetUsersByCity(string city)
+        {
+            try
+            {
+                List<User> users = null;
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create("https://bpdts-test-app.herokuapp.com/city/"+ city + "/users");
+                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+                if (response.StatusCode == HttpStatusCode.OK)
+                {
+                    string responseStream;
+
+                    using (var streamReader = new StreamReader(response.GetResponseStream()))
+                    {
+                        responseStream = streamReader.ReadToEnd();
+                    }
+
+                    var serializer = new JavaScriptSerializer();
+                    users = serializer.Deserialize<List<User>>(responseStream);
+
+                    return Ok(users);
+                }
+            }
+
+            catch (Exception ex)
+            {
+
+                return NotFound();
+            }
+
+            return NoContent();
         }
     }
 }
